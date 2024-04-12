6bd958671ac8a8bb94e025fec0a51685a82dc0b1
Davi Pedro <davisilvaps02@gmail.com>
2024-04-12 11:46:00 -0300

    Creating Medico Entity and small db conn change

---
+++ src/main/java/med/voll/model/Medico.java
@@ -0,0 +1,36 @@
+package med.voll.model;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "medicos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Medico {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String CRM;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+}
