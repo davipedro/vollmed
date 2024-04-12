c18041c63f80ad50b440e81dac0108970f6a3a0d
Davi Pedro <davisilvaps02@gmail.com>
2024-04-12 11:45:11 -0300

    Embeddable class for Endereco

---
+++ src/main/java/med/voll/model/Endereco.java
@@ -0,0 +1,24 @@
+package med.voll.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Endereco {
+    private Long id;
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+}
