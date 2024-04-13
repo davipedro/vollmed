b5970f8fcc1f789af4e9e60bc3f249f44c5d2a5b
Davi Pedro <davisilvaps02@gmail.com>
2024-04-12 22:42:48 -0300

    Adding constructor to Update Endereco on Endereco

--- src/main/java/med/voll/api/model/Endereco.java
+++ src/main/java/med/voll/api/model/Endereco.java
@@ -2,18 +2,17 @@ package med.voll.api.model;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import med.voll.api.dto.DadosEndereco;
 @Getter
 @Setter
 @Embeddable
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = "id")
 public class Endereco {
-    private Long id;
+    
     private String logradouro;
     private String bairro;
     private String cep;
@@ -21,4 +20,16 @@ public class Endereco {
     private String complemento;
     private String cidade;
     private String uf;
+    
+    public Endereco(DadosEndereco endereco) {
+        this.logradouro = endereco.logradouro();
+        this.bairro = endereco.bairro();
+        this.cep = endereco.cep();
+        this.numero = endereco.numero();
+        this.complemento = endereco.complemento();
+        this.cidade = endereco.cidade();
+        this.uf = endereco.uf();
+    }
+
+    
 }
