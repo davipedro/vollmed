ef8542e2d3bdd7323b8d298f2146fc1d56125fb8
Davi Pedro <davisilvaps02@gmail.com>
2024-04-12 22:38:15 -0300

    Adding getMedicos(GET) and atualizar(PUT) methods

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,16 +1,58 @@
 package med.voll.api.controller;
 
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.dto.medico.RegistroMedicoDTO;
+import med.voll.api.dto.medico.ResponseMedicoDTO;
+import med.voll.api.dto.medico.UpdateMedicoDTO;
+import med.voll.api.service.MedicoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
-    
-    @PostMapping
-    public void cadastrar(@RequestBody String json){
 
+    private static final String URI_BASE = "localhost:8080/medicos";
+    
+    @Autowired
+    private MedicoService medicoService;
+
+    @PostMapping("/medico")
+    @Transactional
+    public ResponseEntity<Object> cadastrar(@RequestBody @Valid RegistroMedicoDTO dados){
+        Long medicoId = medicoService.cadastrarMedico(dados);
+
+        String uri = URI_BASE + "/medico" + medicoId;
+
+        return ResponseEntity.created(URI.create(uri)).build();
     }
+
+    @GetMapping
+    public ResponseEntity<Page<ResponseMedicoDTO>> getMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return ResponseEntity.ok().body(medicoService.buscarMedicos(paginacao));
+    }
+
+    @PutMapping("medico/{id}")
+    @Transactional
+    public ResponseEntity<Object> atualizar(@PathVariable Long id, @RequestBody UpdateMedicoDTO medicoUpdate) {
+        medicoService.atualizarDadosMedico(id, medicoUpdate);
+        return ResponseEntity.ok().build();
+    }
+    
 }
