65761e9fd01bdfae3641b7aba8dd8d14016d49d5
Davi Pedro <davisilvaps02@gmail.com>
2024-04-21 10:20:02 -0300

    Changing classes package

--- src/main/java/med/voll/api/dto/medico/ResponseMedicoDTO.java
+++
@@ -1,9 +0,0 @@
-package med.voll.api.dto.medico;
-
-import med.voll.api.model.Especialidade;
-
-public record ResponseMedicoDTO(String nome, 
-                                String email, 
-                                String CRM, 
-                                Especialidade especialidade) {
-}
