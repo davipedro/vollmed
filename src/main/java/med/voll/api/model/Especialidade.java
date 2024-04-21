e86a8d17421dfecb42c2e90ff4e1df60b9e5cd8a
Davi Pedro <davisilvaps02@gmail.com>
2024-04-21 10:20:34 -0300

    Adding Especialidade validation on Especialidade

--- src/main/java/med/voll/api/model/Especialidade.java
+++ src/main/java/med/voll/api/model/Especialidade.java
@@ -1,8 +1,25 @@
 package med.voll.api.model;
 
+import java.util.Arrays;
+
 public enum Especialidade {
-    ORTOPEDIA,
-    CARDIOLOGIA,
-    GINECOLOGIA,
-    DERMATOLOGIA;
+    ORTOPEDIA("Ortopedia"),
+    CARDIOLOGIA("Cardiologia"),
+    GINECOLOGIA("Ginecologia"),
+    DERMATOLOGIA("Dermatologia");
+
+    private final String especialidade;
+
+    Especialidade(String especialidade){
+        this.especialidade = especialidade;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public static boolean ehEspecialidade(Especialidade especialidade){
+        return Arrays.stream(Especialidade.values())
+                .anyMatch(esp -> esp.getEspecialidade().equalsIgnoreCase(especialidade.getEspecialidade()));
+    }
 }
