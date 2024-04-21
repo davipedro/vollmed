e295dfee4dfa12cd1847875da82ea31890cf22c3
Davi Pedro <davisilvaps02@gmail.com>
2024-04-21 10:17:56 -0300

    Usuario Repository fix package import

--- src/main/java/med/voll/api/repository/UsuarioRepository.java
+++ src/main/java/med/voll/api/repository/UsuarioRepository.java
@@ -3,7 +3,7 @@ package med.voll.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import med.voll.api.model.Usuario;
+import med.voll.api.model.usuario.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
