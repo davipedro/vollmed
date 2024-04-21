95c0fad45e23fe4212d723fafd534da23328e458
Davi Pedro <davisilvaps02@gmail.com>
2024-04-21 16:25:37 -0300

    JsonAlias for DadosCancelamento Dto

--- src/main/java/med/voll/api/dto/consulta/DadosCancelamentoConsulta.java
+++ src/main/java/med/voll/api/dto/consulta/DadosCancelamentoConsulta.java
@@ -1,9 +1,12 @@
 package med.voll.api.dto.consulta;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 import jakarta.validation.constraints.NotNull;
 
 public record DadosCancelamentoConsulta(
         @NotNull
+        @JsonAlias("id")
         Long idConsulta,
 
         @NotNull
